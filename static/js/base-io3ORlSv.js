f1af033c21b9b08129d640283e31caea68b701e8
iotjin <179608047@qq.com>
2024-11-06 03:20:38 +0000

    Deploying to gh-pages from @ iotjin/jh-vue3-admin@21a32947c7758b3f9addff88cee652387d56367e 🚀

--- static/js/base-io3ORlSv.js
+++
@@ -1,1 +0,0 @@
-import{g as t,a as e}from"./dict-BYyJu2Xi.js";function n(){return t({type:"dept"})}function s(){return t({type:"status"})}function c(){return t({type:"level"})}function r(){return e({types:"dept,level"})}export{c as a,s as b,r as c,n as g};
