f45f122bce8ed1a63b794fe885e5bd56a613a54f
iotjin <179608047@qq.com>
2025-11-28 08:10:33 +0000

    Deploying to gh-pages from @ iotjin/jh-vue3-admin@6b48e55c44ba2a11a2899192b76ad5569681dce5 🚀

---
+++ static/js/base-BcGN4cv5.js
@@ -0,0 +1,1 @@
+import{g as t,a as e}from"./dict-D2y79XyQ.js";function n(){return t({type:"dept"})}function s(){return t({type:"status"})}function c(){return t({type:"level"})}function r(){return e({types:"dept,level"})}export{c as a,s as b,r as c,n as g};
